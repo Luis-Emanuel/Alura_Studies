11bd1ef4cceb4f02be18d1baebaab338b3678acf
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2024-08-16 03:35:45 -0300

    module css
    Aplicamos module CSS no projeto e utilizamos SASS para estilizar.

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Botao from "../Botao";
+import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form>
-        <div>
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
@@ -15,16 +16,16 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <div>
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
-          <input 
-          type="time"  
-          step="1"
-          name="tempo"
-          id="tempo"
-          min="00:00:00"
-          max="01:30:00"
-          required
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
           />
         </div>
         <Botao />
