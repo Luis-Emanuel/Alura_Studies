102deea5bc8ba631dd4c610204cd76967849d530
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2024-08-21 17:45:03 -0300

    Aplicando state e deixando a aplicação dinâmica
    Controlamos os inputs do componente "Formulario" adicionando um estado para cada um.
    Acrescentamos a props type ao "Botao" dessa forma quando criamos um botão ele terá o seu tipo seja:  "button" | "submit" | "reset" | undefined .
    
    Criamos uma interface para as tarefas assim facilitando a tipagem nos componentes em que utilizamos ela.
    Colocamos o estado da nossa lista de tarefas no componente "App" para que tando a "Lista" quando o" Formulario" tenham acesso ao estado.

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,27 +1,17 @@
-import React from "react";
 import style from "./Lista.module.scss";
+import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
 
-function Lista() {
-  const tarefas = [{
-    tarefa: "React",
-    tempo: "00:15:20"
-  }, {
-    tarefa: "Angular",
-    tempo: "00:25:00"
-  }, {
-    tarefa: "Git",
-    tempo: "01:30:00"
-  }]
-
+function Lista({tarefas}: {tarefas:ITarefa[]}) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li className={style.item} key={index}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item
+            key={index}
+            {...item}
+          />
         ))}
       </ul>
     </aside>
